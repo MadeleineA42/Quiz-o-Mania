3ca658f416905425bf321321147374508dbd24a6
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-07-26 16:31:20 -0400

    took out timer code

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-var secondsLeft = 60;
-
-function setTime()  {
-    var timerInterval = setInterval(function()  {
-        secondsLeft--;
-        timerInterval.textContent = secondsLeft + "Time remaining.";
-
-        if(secondsLeft ===0)    {
-            clearInterval(timerInterval);
-            sendMessage();
-        }
-
-    }, 1000);
-}
-
 const questions = [
     {
         question: "Commonly used data strings DO NOT include ___.",
@@ -59,4 +44,7 @@ const questions = [
             {text: "console.log", correct: true},
         ] 
       }
-]
+];
+
+const question = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
