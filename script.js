c667c973e5f20f1210453873b54edb13f099c084
Madeleine Abraham <mads@Madeleines-MacBook-Pro.local>
2023-08-02 20:41:54 -0400

    formatted doc

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
+// added global vars
 const startScreen = document.querySelector('.main-box');
 const startButton = document.querySelector('#start');
 const question = document.querySelector('#question');
 const answerButton = document.querySelector('#answer-buttons');
 
-
+// these start the timer and tally scores
 let questionIndex = 0;
 let timeRemaining = 75;
 let score = 0;
 
-function startQuiz()  {
+//starts the q's
+function startQuiz() {
   startScreen.innerHTML = ``;
   timeRemaining();
   showQuestion();
-} 
-// startQuiz()
+}
 
 
 
+//these are the quiz q's
 const questions = [
-    {
-        question: "Commonly used data strings DO NOT include ___.",
-        answers: [
-            {text: "strings", correct: false},
-            {text: "booleans", correct: false},
-            {text: "alerts", correct: true},
-            {text: "numbers", correct: false},
-        ]
-    },
-    {
-        question: "Sting values must be enclosed within ___ when being assigned variables",
-        answers: [
-            {text: "commas", correct: false},
-            {text: "curly brackets", correct: false},
-            {text: "quotes", correct: true},
-            {text: "parenthesis", correct: false},
-        ] 
-      },
-      {
-        question: "Arrays in JavaScript can be used to store ___.",
-        answers: [
-            {text: "numbers and strings", correct: false},
-            {text: "other arrays", correct: false},
-            {text: "booleans", correct: false},
-            {text: "all of the above", correct: true},
-        ] 
-      },
-      {
-        question: "The condition in an if/else statement is enclosed with ___.",
-        answers: [
-            {text: "quotes", correct: false},
-            {text: "curly brackets", correct: false},
-            {text: "parenthesis", correct: true},
-            {text: "square brackets", correct: false},
-        ] 
-      },
-      {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: [
-            {text: "JavaScript", correct: false},
-            {text: "terminal/bash", correct: false},
-            {text: "for loops", correct: false},
-            {text: "console.log", correct: true},
-        ] 
-      }
+  {
+    question: "Commonly used data strings DO NOT include ___.",
+    answers: [
+      { text: "strings", correct: false },
+      { text: "booleans", correct: false },
+      { text: "alerts", correct: true },
+      { text: "numbers", correct: false },
+    ]
+  },
+  {
+    question: "Sting values must be enclosed within ___ when being assigned variables",
+    answers: [
+      { text: "commas", correct: false },
+      { text: "curly brackets", correct: false },
+      { text: "quotes", correct: true },
+      { text: "parenthesis", correct: false },
+    ]
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ___.",
+    answers: [
+      { text: "numbers and strings", correct: false },
+      { text: "other arrays", correct: false },
+      { text: "booleans", correct: false },
+      { text: "all of the above", correct: true },
+    ]
+  },
+  {
+    question: "The condition in an if/else statement is enclosed with ___.",
+    answers: [
+      { text: "quotes", correct: false },
+      { text: "curly brackets", correct: false },
+      { text: "parenthesis", correct: true },
+      { text: "square brackets", correct: false },
+    ]
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      { text: "JavaScript", correct: false },
+      { text: "terminal/bash", correct: false },
+      { text: "for loops", correct: false },
+      { text: "console.log", correct: true },
+    ]
+  }
 ];
 
-
+//event listener to start quiz
 startButton.addEventListener('click', startQuiz);
 
 
